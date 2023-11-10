8b235f684d1e036b0a9abeec5ef616b92ae7ccb6
Davi Oliveira <novaes.davi1@gmail.com>
2023-11-10 00:26:36 -0300

    Add project files.

---
+++ Scrap/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EasyAutomationFramework;
+using EasyAutomationFramework.Model;
+using WebScraping;
+
+var web = new WebScraper();
+
+var tablets = web.GetData("https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets");
+
+var dados = new List<DataTables>()
+    {
+        new DataTables("tablets", tablets)
+    };
+
+var paramss = new ParamsDataTable("Dados", @"C:\Users\dnova\source\repos\WebScraping\WebScraping", dados);
+
+Base.GenerateExcel(paramss);
+
---
+++ Scrap/WebScraper.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using WebScraping.Model;
+using EasyAutomationFramework;
+using OpenQA.Selenium;
+
+namespace WebScraping
+{
+    public class WebScraper : Web
+    {
+        public DataTable GetData(string link)
+        {
+                StartBrowser(TypeDriver.GoogleChorme);
+
+            var items = new List<Item>();
+
+            Navigate(link);
+
+            var elements = GetValue(TypeElement.Xpath, "/html/body/div[1]/div[3]/div/div[2]/div").element.FindElements(By.ClassName("thumbnail"));
+            
+            foreach (var element in elements)
+            {
+                var item = new Item();
+                item.Title = element.FindElement(By.ClassName("title")).GetAttribute("title");
+                item.Price = element.FindElement(By.ClassName("price")).Text;
+                item.Description = element.FindElement(By.ClassName("description")).Text;
+                items.Add(item); 
+
+
+            }
+
+            return Base.ConvertTo(items);
+
+        }
+    }
+}
