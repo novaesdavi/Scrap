b69cd45c69e2d12ec46d38f4840038c61e82f474
Davi Oliveira <novaes.davi1@gmail.com>
2023-11-11 19:44:46 -0300

    d

---
+++ Scrap/InitChromeOnDebugMode.bash
@@ -0,0 +1,2 @@
+        cd "C:\Program Files (x86)\Google\Chrome\Application"
+        .\chrome.exe --remote-debugging-port=9100 --user-data-dir="C:\Users\dnova\AppData\Local\Google\Chrome\User Data\Default\Cache\Cache_Data"
--- Scrap/LatamScraper.cs
+++ Scrap/LatamScraper.cs
@@ -15,8 +15,6 @@ namespace WebScraping
         public LatamScraper()
         {
         }
-        //cd "C:\Program Files (x86)\Google\Chrome\Application"
-        // .\chrome.exe --remote-debugging-port=9100 --user-data-dir="C:\Users\dnova\AppData\Local\Google\Chrome\User Data\Default\Cache\Cache_Data"
         private void InitBrowser(string link)
         {
             var options = new ChromeOptions();
